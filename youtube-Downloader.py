70cc607f7ea86afaa6ac2400111a5b752fec6858
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-07-03 11:24:44 -0700

    Added streamlit code for input

--- youtube-Downloader.py
+++ youtube-Downloader.py
@@ -1,13 +1,36 @@
 from pytube import YouTube
+import streamlit as st
 print("Youtube Downloader")
-yt = YouTube('https://www.youtube.com/watch?v=zWWniuV8juI&ab_channel=FOXSoccer')
-print(yt.title)
-print(yt.thumbnail_url)
+
+
+
+
+# Title of the app
+st.title('Youtube Downloader')
+
+# Ask the user for a string input
+user_input = st.text_input("Enter URL You need Info Of: ")
+submitted = st.button('Submit')
+
+if submitted:
+    if not user_input:
+        st.error('Error: Input cannot be empty.')
+    else:
+        # If no errors, display the input string
+        try:
+            yt = YouTube(user_input)
+            st.success("Parsed the URL successfully")
+        except Exception as e:
+            st.error("Error in parsing URL, please check URL")
+            print("Error: ",e)
+
+
+
 
 import requests
 import shutil
 
-def download_image(url, file_name):
+'''def download_image(url, file_name):
     res = requests.get(url, stream = True)
 
     if res.status_code == 200:
@@ -18,4 +41,4 @@ def download_image(url, file_name):
         print('Image Couldn\'t be retrieved')
 print("Running Image download")
 # Call the function
-download_image(yt.thumbnail_url, 'download.jpg')
+download_image(yt.thumbnail_url, 'download.jpg')'''
